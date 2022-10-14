7fd417a8dd1e798b7c88410fd5ea89752175da3e
Smail93110 <smailouldbey93pro@gmail.com>
2022-10-14 21:17:10 +0200

    delete

--- delete.php
+++ delete.php
@@ -5,6 +5,10 @@ $objetPdo = new PDO('mysql:host=localhost;dbname=bloc_note', 'root', '');
 $pdoStat =$objetPdo->prepare('DELETE FROM tasks WHERE id=:id');
 $pdoStat->bindValue(':id', $_GET['id']);
 
+
+
+
+
 $deleteIsOk = $pdoStat->execute();
 
 if ($deleteIsOk) {
